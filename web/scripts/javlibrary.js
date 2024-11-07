37aeeed632807e31a90260d56a7b78976a623fbd
ln613 <ln613@hotmail.com>
2024-11-07 14:42:52 -0800

    sort

--- web/scripts/javlibrary.js
+++ web/scripts/javlibrary.js
@@ -1,5 +1,8 @@
+import { sortBy } from 'lodash'
 import { Url, value, click, $1, $3 } from './html.js'
 import { wrapRD, trimUrl, rgSize } from './utils.js'
+
+const sorts = ['https://pics.dmm.co.jp', 'Reducing_Mosaic', 'Mosaic_reduce', 'rapidgator.net']
 
 const m = Url.params.m
 
@@ -14,7 +17,13 @@ if (all) {
   click(all)
 } else {
   $3('.showalltext').forEach(click)
+  $1('#video_comments').innerHTML = sortBy($3('table.comment'), [order]).map(x => x.outerHTML).join('')
   trimUrl('https://www.javlibrary.com/en/redirect.php?url=')
   wrapRD()
   rgSize()
+}
+
+const order = x => {
+  const i = sorts.findIndex(y => x.innerText.includes(y))
+  return i == -1 ? 100 : i
 }
