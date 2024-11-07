932da999eafba84a58647e84507c659b045c4c73
ln613 <ln613@hotmail.com>
2024-11-07 12:40:09 -0800

    id search

--- web/scripts/javlibrary.js
+++ web/scripts/javlibrary.js
@@ -1,4 +1,12 @@
+import { Url, val, click } from './html.js'
 import { wrapRD, trimUrl, rgSize } from './utils.js'
+
+const m = Url.params.m
+
+if (m) {
+  val('#idsearchbox', m)
+  click('#idsearchbutton')
+}
 
 trimUrl('https://www.javlibrary.com/en/redirect.php?url=')
 
