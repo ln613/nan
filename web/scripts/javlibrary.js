9018d7081bad049ddfb6aa88bd84e8f3032d4006
ln613 <ln613@hotmail.com>
2024-11-07 17:03:46 -0800

    _

--- web/scripts/javlibrary.js
+++ web/scripts/javlibrary.js
@@ -1,4 +1,3 @@
-import { sortBy } from 'lodash'
 import { Url, value, click, $1, $3 } from './html.js'
 import { wrapRD, trimUrl, rgSize } from './utils.js'
 
@@ -17,7 +16,7 @@ if (all) {
   click(all)
 } else {
   $3('.showalltext').forEach(click)
-  $1('#video_comments').innerHTML = sortBy($3('table.comment'), [order]).map(x => x.outerHTML).join('')
+  $1('#video_comments').innerHTML = _.sortBy($3('table.comment'), [order]).map(x => x.outerHTML).join('')
   trimUrl('https://www.javlibrary.com/en/redirect.php?url=')
   wrapRD()
   rgSize()
