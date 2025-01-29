0b25f93698da824a6239aed92bcd5fc778c9be2b
ln613 <ln613@hotmail.com>
2025-01-28 16:09:39 -0800

    oauth

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { OAuth } from './components/oauth.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(<OAuth><App /></OAuth>)
