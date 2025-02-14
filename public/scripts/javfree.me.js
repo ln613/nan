3cf2dc93364213ef8f59852ad4f2437bdfb97b7e
ln613 <ln613@hotmail.com>
2025-02-14 02:28:57 -0800

    csp

--- public/scripts/javfree.me.js
+++ public/scripts/javfree.me.js
@@ -1,4 +1,18 @@
+import { $1, $3 } from './html.js'
 import { wrapLA, rgSize } from './utils.js'
 
 wrapLA()
 rgSize()
+
+$1('#content').style.margin = 0
+const imgs = $3('.entry-content p img')
+const imgsAdded = []
+imgs.forEach((x, i) => {
+  const src = x.src.replace('-4k.h264', '').replace('-8k.hevc', '').replace('-4k', '').replace('-8k', '')
+  if (imgsAdded.includes(src)) {
+    x.style.display = 'none'
+  } else {
+    x.style.maxWidth = 'fit-content' 
+    imgsAdded.push(src)
+  }
+})
--- public/scripts/leechall.js
+++ public/scripts/leechall.js
@@ -1,19 +1,25 @@
-import { $1, css, append, remove, text, value, click, Url } from './html.js'
+import { $1, css, append, remove, text, value, click, Url, waitFor } from './html.js'
 
 const m = Url.params.m
 
-if (Url.pathname === '/downloader') {
-  if (m) {
-    $1('#urllist').focus()
-    text('#urllist', m)
-    $1('#password').focus()
-    setTimeout(() => click('.btn.mr-2.btn-gradient-primary'), 1000)
-    const iid = setInterval(() => {
-      if ($1('.card.bg-primary')) {
-        clearInterval(iid)
-        window.location.href = $1('.card.bg-primary a').href
-        //setTimeout(window.close, 2000)
-      }
-    }, 100)
+const run = async () => {
+  if (Url.pathname === '/downloader') {
+    if (m) {
+      const t = '#urllist'
+      await waitFor(t)
+      $1(t).focus()
+      text(t, m)
+      $1('#password').focus()
+      setTimeout(() => click('.btn.mr-2.btn-gradient-primary'), 1000)
+      const iid = setInterval(() => {
+        if ($1('.card.bg-primary')) {
+          clearInterval(iid)
+          window.location.href = $1('.card.bg-primary a').href
+          //setTimeout(window.close, 2000)
+        }
+      }, 100)
+    }
   }
 }
+
+run()
